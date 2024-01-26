e3b7cd6e827d7632dcc061d2240a0e6b80dd5856
Tianwei Liu <tianwei.liu@outlook.com>
2024-01-26 11:16:16 -0500

    main: prompt on numbers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::{fs::File, io::Write};
 use rand::Rng;
 fn main() {
-    for i in 0..10 {
+    let mut str = String::new();
+    println!("How many files?");
+    std::io::stdin().read_line(&mut str).expect("Cannot Read Line");
+    let f_num: u32 = str.trim().parse().expect("Not a number!");
+    str = String::new();
+    println!("How many track in each file?");
+    std::io::stdin().read_line(&mut str).expect("Cannot Read Line");
+    let t_num: u32 = str.trim().parse().expect("Not a number!");
+    for i in 0..f_num {
         let mut file = File::create(format!("disk.in{}", i)).unwrap();
         let mut rng = rand::thread_rng();
-        for i in 0..10 {
+        for _ in 0..t_num {
             let rn = rng.gen_range(0..=999);
             writeln!(file, "{}", rn).unwrap();
         }
