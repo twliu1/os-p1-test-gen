3dac73e8aa093d578c150b7afde1e0fdf51d21a0
Tianwei Liu <tianwei.liu@outlook.com>
2024-01-31 10:28:57 -0500

    nit: format

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,17 @@ use rand::Rng;
 fn main() {
     let mut str = String::new();
     println!("How many files?");
-    std::io::stdin().read_line(&mut str).expect("Cannot Read Line");
+    std::io::stdin().read_line(&mut str).expect("Cannot read line!");
     let f_num: u32 = str.trim().parse().expect("Not a number!");
     str = String::new();
     println!("How many track in each file?");
-    std::io::stdin().read_line(&mut str).expect("Cannot Read Line");
+    std::io::stdin().read_line(&mut str).expect("Cannot read line!");
     let t_num: u32 = str.trim().parse().expect("Not a number!");
     for i in 0..f_num {
         let mut file = File::create(format!("disk.in{}", i)).unwrap();
         let mut rng = rand::thread_rng();
         for _ in 0..t_num {
-            let rn = rng.gen_range(0..=999);
-            writeln!(file, "{}", rn).unwrap();
+            writeln!(file, "{}", rng.gen_range(0..1000)).unwrap();
         }
     }
-
 }
